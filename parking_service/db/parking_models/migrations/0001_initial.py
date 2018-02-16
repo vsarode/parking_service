1e20b0174be0f4cb8dfc4ed774ba7100808dfb9c
vsarode <vitthal.sarode1@gmail.com>
2018-02-16 03:21:37 +0000

    added new django migrations

--- parking_service/db/parking_models/migrations/0001_initial.py
+++ parking_service/db/parking_models/migrations/0001_initial.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ParkingBlock',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('block_code', models.CharField(max_length=124, serialize=False, primary_key=True)),
                 ('is_free', models.BooleanField(default=False)),
                 ('parking', models.ForeignKey(to='parking_models.Parking')),
             ],
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
             name='ParkingEntry',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('start_time', models.DateTimeField(default=datetime.datetime(2018, 2, 15, 12, 23, 23, 76117))),
+                ('start_time', models.DateTimeField(default=datetime.datetime(2018, 2, 15, 21, 20, 33, 994772))),
                 ('end_time', models.DateTimeField()),
                 ('parking_block', models.ForeignKey(to='parking_models.ParkingBlock')),
             ],
@@ -42,7 +42,7 @@ class Migration(migrations.Migration):
                 ('password', models.CharField(max_length=256)),
                 ('mobile_no', models.CharField(max_length=256)),
                 ('address', models.CharField(max_length=256, null=True)),
-                ('created_on', models.DateTimeField(default=datetime.datetime(2018, 2, 15, 12, 23, 23, 74366))),
+                ('created_on', models.DateTimeField(default=datetime.datetime(2018, 2, 15, 21, 20, 33, 992947))),
             ],
         ),
         migrations.AddField(
