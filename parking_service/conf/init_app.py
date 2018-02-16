cd76bae38edf49c53a22c61651d3fa68b08074b8
vsarode <vitthal.sarode1@gmail.com>
2018-02-16 03:26:04 +0000

    Sql alchemy removeed

--- parking_service/conf/init_app.py
+++ parking_service/conf/init_app.py
@@ -1,9 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 
 app = Flask(__name__)
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'mysql://root:as2d2p@127.0.0.1/parkingDb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
