013e684336738f017bdbfabf66e1aa6e6fcc5f81
vsarode <vitthal.sarode1@gmail.com>
2018-02-19 15:21:27 +0000

    user oobject error

--- parking_service/service_apis/login.py
+++ parking_service/service_apis/login.py
@@ -1,17 +1,18 @@
 from flask import request, jsonify
-from flask_restful import Resource
 from parking_service.service_api_handlers import post_login_handler
+from parking_service.utils.exceptions import UnauthorisedException
+from parking_service.utils.resource import BaseResource
 from parking_service.views.user import UserView
 
 
-class Login(Resource):
+class Login(BaseResource):
     def post(self):
         request_data = request.get_json(force=True)
         view = UserView()
         response = post_login_handler.handle_post(request_data)
         if response:
-            return jsonify(view.render(response))
+            return {'user': view.render(response)}
         else:
-            return jsonify({'login': "Failure"})
+            raise UnauthorisedException()
 
     post.authenticated = False
