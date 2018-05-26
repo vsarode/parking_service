e029bf4e285c2d17cc65c84a576a745c3448b8f5
mahadev <mahadev@thoughtworks.com>
2018-05-26 11:11:47 +0530

    add start.sh to start service

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#export PYTHONPATH=$pwd
+export DJANGO_SETTINGS_MODULE=parking_service.db.settings.local
+python parking_service/manage.py makemigrations
+python parking_service/manage.py migrate
+python parking_service/conf/service_app.py
